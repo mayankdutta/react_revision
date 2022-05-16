7164fc15781c477b1b06b4c0f09e31275359134e
mayank dutta <smayankdutt@gmail.com>
2022-05-17 01:24:57 +0530

    some pending changes

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,6 +1,7 @@
-const AddItem = () => {
+const AddItem = ({newItem, setNewItem, handleSubmit}) => {
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
+            {/*  we need not mention event in case of onSubmit it is implicitly passed.*/}
             <label htmlFor={'addItem'}>Add Item</label>
             <input
                 autoFocus
@@ -8,11 +9,17 @@ const AddItem = () => {
                 type={'text'}
                 placeholder={"Add item"}
                 required
+                /*
+                 * we need to apply onChange to change value,
+                 * otherwise it won't be changed
+                 */
+                value={newItem}
+                onChange={e => setNewItem(e.target.value)}
             />
             <button
                 type={"submit"}
                 aria-label={'Add Item'}
-            > Submit
+            > +
             </button>
 
         </form>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Content from "./content";
-import Header from "./Header"
+import Header from "./Header";
 import AddItem from "./AddItem";
 import {useState} from "react";
 
@@ -9,33 +9,50 @@ function App() {
         {index: 2, title: "b", checked: false},
         {index: 3, title: "c", checked: false},
     ]);
+    const [newItem, setNewItem] = useState("initial state");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // we are adding object in the array.
+        // first spread the original array.
+        // and then add object to it, the object that array currently supports.
+
+        setItems(prev => [
+            ...prev, {
+                index: items.length + 1,
+                title: newItem,
+                checked: false
+            }
+        ]);
+        localStorage.setItem('shoppingList', JSON.stringify(items));
+    };
 
     const add = (index) => {
-
-    }
+    };
 
     const del = (i) => {
-        const updatedItem = items.filter(item => item.index !== i)
-        setItems(
-            updatedItem
-        )
+        const updatedItem = items.filter((item) => item.index !== i);
+        setItems(updatedItem);
         localStorage.setItem("shoppingList", JSON.stringify(updatedItem));
-    }
+    };
 
     const checked = (i) => {
-        const updatedItems =
-            items.map(item =>
-                item.index === i ? {...item, checked: !item.checked} : item)
-        setItems(
-            updatedItems
-        )
+        const updatedItems = items.map((item) =>
+            item.index === i ? {...item, checked: !item.checked} : item
+        );
+        setItems(updatedItems);
         localStorage.setItem("shoppingList", JSON.stringify(updatedItems));
-    }
+    };
     return (
         <>
             <Header title="Grocery List"/>
-            <AddItem />
-            <Content items={items} checked={checked} del = {del}/>
+            <AddItem
+                newItem={newItem}
+                setNewItem={setNewItem}
+                handleSubmit={handleSubmit}
+            />
+            <Content items={items} checked={checked} del={del}/>
         </>
     );
 }
